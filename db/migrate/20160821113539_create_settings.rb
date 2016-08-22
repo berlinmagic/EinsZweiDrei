d14da7c4421553db8076caa2a77c6169880253df
Torsten Wetzel <torsten@orderlift.com>
2016-08-23 01:07:20 +0200

    add infos

--- db/migrate/20160821113539_create_settings.rb
+++ db/migrate/20160821113539_create_settings.rb
@@ -6,7 +6,7 @@ class CreateSettings < ActiveRecord::Migration
       
       t.decimal     :blink_time,    default: 10.0           # selectTime          => 1000 * x
       t.decimal     :stop_time,     default: 3.0            # showTime            => 1000 * x
-      t.integer     :interval_time, default: 120            # intervalTime        => x
+      t.integer     :interval_time, default: 150            # intervalTime        => x
       t.integer     :speed_step,    default: 20             # intervalStep        => x
       t.integer     :step_time,     default: 1000           # intervalStepEvery   => x
       t.string      :step_type,     default: "none"         # none | faster | slower
